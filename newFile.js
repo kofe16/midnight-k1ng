75cf65b007c4faa004775b4fd14b40b69998e3e0
kofe16 <kofekevin16@gmail.com>
2024-06-26 02:57:38 -0500

    primer commit

--- newFile.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const products = document.querySelectorAll(".product");
-
-    products.forEach(product => {
-        product.style.flex = "1 1 calc(20% - 1rem)"; // Ajusta el tamaño de los productos
-    });
-});
