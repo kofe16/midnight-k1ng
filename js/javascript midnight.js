15c598159323ff4076079c86db4280ca917ed9fa
kofinho <165713449+kofe16@users.noreply.github.com>
2024-06-29 04:30:11 +0000

    0

--- js/javascript midnight.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulario enviado');
-    // Aquí puedes añadir lógica para enviar el formulario usando AJAX o fetch
-});
